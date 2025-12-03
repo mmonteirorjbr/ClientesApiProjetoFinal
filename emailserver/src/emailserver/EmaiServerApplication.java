0335ad81c37a18fe1441408c39663721aae1913c
mmonteirorjbr <mmonteiro.rj.br@gmail.com>
2025-12-02 23:53:14 -0300

    Inicio do e-mail server

---
+++ emailserver/src/emailserver/EmaiServerApplication.java
@@ -0,0 +1,14 @@
+package emailserver;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class EmaiServerApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(EmailServerApplication.class, args);
+	}
+
+}
